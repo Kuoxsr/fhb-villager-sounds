6828b21a0b895b531bbe216859233600d2a446a4
Kuoxsr <kuoxsr@gmail.com>
2024-01-25 13:13:53 -0600

    Add the ability to generate packs that allow sounds from lower packs
    This commit allows a user to choose whether a pack will be a standard
    pack or an "addon" pack. Standard packs suppress sounds for each event
    in lower packs.  Addon packs allow all sounds from lower packs.
    
    In my earlier testing, I was able to show that by putting the "loaded"
    edition on the bottom of the list and both "adult" and "uwu" addon packs
    above it, that I could get sounds from all three packs with no side
    effects.
    
    This commit is an initial proof-of-concept commit and has not been
    tested in a Minecraft world. My testing consisted of looking at the list
    of files and the sounds.json file to determine if everything looked ok.
    I am checking it in now just to preserve my work before refactoring.
    
    The refactoring I intend to do will remove the requirement to manually
    choose the pack type, because now that I've added this, in order to
    create the entire set of releases, I would need to run the
    create-release.sh script SIX TIMES!
    
    This contributes to issue #3

--- create-release.sh
+++ create-release.sh
@@ -21,18 +21,69 @@ case $edition in
 esac
 
 echo
+echo
 read -p "Version suffix?  (e.g., 'v1.0): " version
 
 # Create date folder
 date_stamp=$(date +%Y.%m.%d)
 mkdir -p "releases/${date_stamp}"
 
-echo -e "\nPacking $name edition, version $version into folder $date_stamp: \n"
+# Determine whether to create an addon pack
+type=1
+echo -e "\n\nPack types:\n"
+echo "1. Standard pack (default)"
+echo "2. Addon pack"
+echo
+read -e -p "Choose what type of pack to generate: " input
+type=${input:-$type}
+echo
+
+type_name=""
+case $type in
+    1) echo "you have chosen standard pack type" ;;
+    2) echo "you have chosen addon pack type" ;;
+    *) echo "Unknown choice.  Script terminating."; exit 1; ;;
+esac
+
+# the directory of the script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# the temp directory used, within $DIR
+# omit the -p parameter to create a temporary directory in the default location
+WORK_DIR=`mktemp -d -p "$DIR"`
+
+# check if temporary dir was created
+if [[ ! "$WORK_DIR" || ! -d "$WORK_DIR" ]]; then
+  echo "Could not create temp dir"
+  exit 1
+fi
+
+# deletes the temp directory
+function cleanup {
+  rm -rf "$WORK_DIR"
+  echo -e "\nDeleted temp working directory $WORK_DIR"
+}
+
+# register the cleanup function to be called on the EXIT signal
+trap cleanup EXIT
+
+# Report that we're starting the process
+echo -e "\nPacking $name edition, version $version into folder $date_stamp:\n"
+
+# Copy the files to zip into the temporary folder
+cp -a $name/. $WORK_DIR
+
+# If we're building an addon pack, set replace:true to false instead
+pack_type="-edition"
+if [[ "$type" -eq 2 ]]; then
+    pack_type="-addon"
+    sed -i 's/"replace":true,/"replace":false,/g' $WORK_DIR/assets/minecraft/sounds.json
+fi
 
 # In order to prevent the inclusion of every folder from the root
 # to my desired directory, I need to change directory (cd) first:
-filename=fhbvs-$name-edition-$version.zip
-(cd $name && zip -r ../releases/${date_stamp}/$filename .)
+filename=fhbvs-$name$pack_type-$version.zip
+(cd $WORK_DIR && zip -r ../releases/${date_stamp}/$filename .)
 
 # Create a cryptographic hash checksum of our file
 (cd releases/${date_stamp} && sha256sum $filename > $filename.sha256)
